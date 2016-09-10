63e754323badc66ae7dc0e5c6604c3e22dfe93ef
George Saieed <saieed.george0@gmail.com>
2016-09-10 13:33:10 -0400

    add navs

--- new/js/script.js
+++ new/js/script.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
     $('.welcome').css('top', welcomeheight.toString()+"px");
     var meheight = ($(window).height() - $('#me').height())/2;
     $('#me').css('top', meheight.toString()+"px");
-    var arrowsheight = ($(window).height() - $('#arrows').height())/2;
+    var arrowsheight = ($(window).height() - $('#arrows').height() - 97)/2;
     $('#arrows').css('top', arrowsheight.toString()+"px");
     var meleft = $(window).width() - $('#me').width() - (.08*$(window).width());
     $('#me').css('left', meleft.toString()+"px");
     var socmedialeft = ($(window).width() - $('#socialmedia').width() - 107)/2;
     $('#socialmedia').css('left', socmedialeft.toString()+"px");
+    var navcircles = ($(window).width() - $('#move-circles').width() + 30)/2;
+    $('#move-circles').css('left', navcircles.toString()+"px");
     $(".cssload-container").fadeOut(3000);
     $(window).resize(function(){
         $(".cssload-container").css("display", "block");
@@ -16,11 +18,27 @@ $(document).ready(function(){
         $('.welcome').css('top', welcomeheight.toString()+"px");
         meheight = ($(window).height() - $('#me').height())/2;
         $('#me').css('top', meheight.toString()+"px");
+        arrowsheight = ($(window).height() - $('#arrows').height())/2;
+        $('#arrows').css('top', arrowsheight.toString()+"px");
         meleft = $(window).width() - $('#me').width() - (.08*$(window).width());
         $('#me').css('left', meleft.toString()+"px");
-        socmedialeft = ($(window).width() - $('#socialmedia').width() + 59)/2;
+        socmedialeft = ($(window).width() - $('#socialmedia').width())/2;
         $('#socialmedia').css('left', socmedialeft.toString()+"px");
+        navcircles = ($(window).width() - $('#move-circles').width() + 30)/2;
+        $('#move-circles').css('left', navcircles.toString()+"px");
         $(".cssload-container").fadeOut(3000);
+    });
+    
+    $('.fa-circle-o').hover(function () {
+            var labelnum = "#" + "label-" + $(this).attr('id');
+            $(labelnum.toString()).fadeIn(100);
+            $(this).removeClass('fa-circle-o');
+            $(this).addClass('fa-circle');
+        }, function () {
+            var labelnum = "#" + "label-" + $(this).attr('id');
+            $(labelnum.toString()).fadeOut(100);
+            $(this).removeClass('fa-circle');
+            $(this).addClass('fa-circle-o');
     });
 });
                   
