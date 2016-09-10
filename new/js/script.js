509832894d24614b71f0594ad50fe36af087ba59
George Saieed <saieed.george0@gmail.com>
2016-09-10 01:39:13 -0400

    begin new website

---
+++ new/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    var welcomeheight = ($(window).height() - $('.welcome').height())/2;
+    $('.welcome').css('top', welcomeheight.toString()+"px");
+    var meheight = ($(window).height() - $('#me').height())/2;
+    $('#me').css('top', meheight.toString()+"px");
+    var meleft = $(window).width() - $('#me').width() - (.08*$(window).width());
+    $('#me').css('left', meleft.toString()+"px");
+    var socmedialeft = ($(window).width() - $('#socialmedia').width() - 59)/2;
+    $('#socialmedia').css('left', socmedialeft.toString()+"px");
+    $(".cssload-container").fadeOut(3000);
+    $(window).resize(function(){
+        $(".cssload-container").css("display", "block");
+        welcomeheight = ($(window).height() - $('.welcome').height())/2;
+        $('.welcome').css('top', welcomeheight.toString()+"px");
+        meheight = ($(window).height() - $('#me').height())/2;
+        $('#me').css('top', meheight.toString()+"px");
+        meleft = $(window).width() - $('#me').width() - (.08*$(window).width());
+        $('#me').css('left', meleft.toString()+"px");
+        socmedialeft = ($(window).width() - $('#socialmedia').width()+59)/2;
+        $('#socialmedia').css('left', socmedialeft.toString()+"px");
+        $(".cssload-container").fadeOut(3000);
+    });
+});
+                  
