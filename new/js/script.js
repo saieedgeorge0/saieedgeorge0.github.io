e0908bef9f74da51889edfb343e8874ba1adb522
George Saieed <saieed.george0@gmail.com>
2016-09-14 12:28:10 -0400

    finish adding imgs

--- new/js/script.js
+++ new/js/script.js
@@ -11,18 +11,18 @@ $(document).ready(function(){
     $('#socialmedia').css('left', socmedialeft.toString()+"px");
     var navcircles = ($(window).width() - $('#move-circles').width() + 45)/2;
     $('#move-circles').css('left', navcircles.toString()+"px");
-    $('#container').css("background-image", "url('img/eduuc.jpg')");
-    $('#container').css("background-position", "center");
-    $('#container').css("background-repeat", "none");
-    $('#container').css("background-size", "cover");
-    $('#container').css("background-image", "url('img/edust.jpg')");
-    $('#container').css("background-position", "center");
-    $('#container').css("background-repeat", "none");
-    $('#container').css("background-size", "cover");
-    $('#container').css("background-image", "url('img/background.jpeg')");
-    $('#container').css("background-position", "center");
-    $('#container').css("background-repeat", "none");
-    $('#container').css("background-size", "cover");
+    this.addEventListener("DOMContentLoaded", preloadImages, true);
+ 
+    function preloadImages(e) {
+        var imageArray = new Array("img/background.jpeg", "img/blogbike.jpg", "img/blogbwc.jpeg", "img/blogdaw.jpeg", "img/bloglight.jpg", "img/edust.jpg", "img/eduuc.jpg", "img/expes.jpg", "img/expic.jpg", "img/expsa.jpg", "img/expupmc.jpg", "img/lowpoly.jpg", "img/me.jpg", "img/projaig.jpg", "img/projcc.jpg", "img/projcw.jpg", "img/projfg.jpg", "img/projfr.png", "img/projid.jpg", "img/projlpa.jpg", "img/projpd.jpeg", "img/projpw.png", "img/projsr.jpg", "img/projuh.jpeg", "img/projuw.jpg");
+
+        for (var i = 0; i < imageArray.length; i++) {
+            var tempImage = new Image();
+
+            tempImage.addEventListener("load", trackProgress, true);
+            tempImage.src = imageArray[i];
+        }
+    }
     
     $(window).on("load", function() {
         $(".cssload-container").delay(1000).fadeOut(3000);
@@ -66,10 +66,13 @@ $(document).ready(function(){
     var edupics = ["url('img/eduuc.jpg')", "url('img/edust.jpg')"];
     var exptext = ["Inventory Connection", "ExplORer Surgical", "Sewickley Academy", "UPMC Hospital"];
     var expsubtext = ["Full Stack Designer + Developer | Jun. 2016 - Present", "Technical Operations Intern | May 2016 - Present", "IT Assistant | Aug. 2015 - Sept. 2015", "Research Volunteer | Jul. 2014 - Aug. 2015"];
+    var exppics = ["url('img/expic.jpg')", "url('img/expes.jpg')", "url('img/expsa.jpg')", "url('img/expupmc.jpg')"];
     var projtext = ["Church Website", "Facebook Re-Design", "Accident Report", "Smart Receipts", "Finals Generator", "Low Polygon Art", "Presentation Design", "Calc Color", "Instagram Logo Design", "Ultimate Horn", "UW Poster", "Past Websites"];
     var projsubtext = ["Made w/ HTML, CSS, jQuery | Sept. 2016", "Made w/ Sketch | Sept. 2016", "Made w/ Sketch | Aug. 2016", "Made w/ Sketch, Swift, XCode | Mar. 2016", "Made w/ Twilio, Python | May 2016", "Made w/ Photoshop | May 2016", "Made w/ Sketch | Jun. 2016", "Made w/ Sketch, Swift, XCode | Jul. 2016", "Made w/ Sketch | May 2016", "Made w/ Swift, XCode | Jul. 2016", "Made w/ Photoshop | May 2016", "Made w/ HTML, CSS, jQuery | Mar./Aug. 2016"];
+    var projpics = ["url('img/projcw.jpg')", "url('img/projfr.png')", "url('img/projaig.jpg')", "url('img/projsr.jpg')", "url('img/projfg.jpg')", "url('img/projlpa.jpg')", "url('img/projpd.jpeg')", "url('img/projcc.jpg')", "url('img/projid.jpg')", "url('img/projuh.jpeg')", "url('img/projuw.jpg')", "url('img/projpw.png')"];
     var blogtext = ["Why am I 'terrible' with computers?", "How do mobile apps really work?", "Could you power your house with your bike?", "How much fun would it be to run at the speed of light?"];
     var blogsubtext = ["March 22\, 2016 | 3 - 5 min. read", "July 14\, 2015 | 8 - 14 min. read", "July 15\, 2016 | 2 - 3 min. read", "July 18\, 2016 | 1-3 min. read"];
+    var blogpics = ["url('img/blogbwc.jpeg')", "url('img/blogdaw.jpeg')", "url('img/blogbike.jpg')", "url('img/bloglight.jpg')"];
     var abouttext = ["So Who Am I?"];
     var textnum = 0;
     
@@ -112,6 +115,10 @@ $(document).ready(function(){
             $('#welcome-description').html(exptext[textnum]);
             $('#welcome-text').hide().delay(50).fadeIn('slow');
             $('#welcome-text').html(expsubtext[textnum]);
+            $('#container').css("background", exppics[textnum]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         if ($('#welcome-text').hasClass('4')) {
             textnum += 1;
@@ -122,6 +129,10 @@ $(document).ready(function(){
             $('#welcome-description').html(projtext[textnum]);
             $('#welcome-text').hide().delay(50).fadeIn('slow');
             $('#welcome-text').html(projsubtext[textnum]);
+            $('#container').css("background", projpics[textnum]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         if ($('#welcome-text').hasClass('5')) {
             textnum += 1;
@@ -129,9 +140,13 @@ $(document).ready(function(){
                 textnum = 0;
             }
             $('#welcome-description').hide().delay(50).fadeIn('slow');
-            $('#welcome-description').html(projtext[textnum]);
+            $('#welcome-description').html(blogtext[textnum]);
             $('#welcome-text').hide().delay(50).fadeIn('slow');
-            $('#welcome-text').html(projsubtext[textnum]);
+            $('#welcome-text').html(blogsubtext[textnum]);
+            $('#container').css("background", blogpics[textnum]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
     });
     
@@ -171,6 +186,10 @@ $(document).ready(function(){
             $('#welcome-description').html(exptext[textnum]);
             $('#welcome-text').hide().delay(50).fadeIn('slow');
             $('#welcome-text').html(expsubtext[textnum]);
+            $('#container').css("background", exppics[textnum]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         if ($('#welcome-text').hasClass('4')) {
             textnum -= 1;
@@ -181,6 +200,10 @@ $(document).ready(function(){
             $('#welcome-description').html(projtext[textnum]);
             $('#welcome-text').hide().delay(50).fadeIn('slow');
             $('#welcome-text').html(projsubtext[textnum]);
+            $('#container').css("background", projpics[textnum]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         if ($('#welcome-text').hasClass('5')) {
             textnum -= 1;
@@ -191,6 +214,10 @@ $(document).ready(function(){
             $('#welcome-description').html(blogtext[textnum]);
             $('#welcome-text').hide().delay(50).fadeIn('slow');
             $('#welcome-text').html(blogsubtext[textnum]);
+            $('#container').css("background", blogpics[textnum]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
     });
     
@@ -213,7 +240,7 @@ $(document).ready(function(){
         if ($(this).attr('id') == "2") {
             $('#welcome-description').html(edutext[0]);
             $('#welcome-description').addClass('huge');
-            $('#back-overlay').css("background", "rgba(0,0,0,0.4)");
+            $('#back-overlay').css("background", "rgba(0,0,0,0.5)");
             $('#container').css("background", edupics[0]);
             $('#container').css("background-position", "center");
             $('#container').css("background-repeat", "none");
@@ -222,18 +249,38 @@ $(document).ready(function(){
         if ($(this).attr('id') == "3") {
             $('#welcome-description').html(exptext[0]);
             $('#welcome-description').addClass('huge');
+            $('#back-overlay').css("background", "rgba(0,0,0,0.5)");
+            $('#container').css("background", exppics[0]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         if ($(this).attr('id') == "4") {
             $('#welcome-description').html(projtext[0]);
             $('#welcome-description').addClass('semi-huge');
+            $('#back-overlay').css("background", "rgba(0,0,0,0.5)");
+            $('#container').css("background", projpics[0]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         if ($(this).attr('id') == "5") {
             $('#welcome-description').html(blogtext[0]);
-            $('#welcome-description').addClass('not-as-huge');
+            $('#welcome-description').addClass('semi-huge');
+            $('#back-overlay').css("background", "rgba(0,0,0,0.5)");
+            $('#container').css("background", blogpics[0]);
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         if ($(this).attr('id') == "6") {
             $('#welcome-description').html(abouttext[0]);
             $('#welcome-description').addClass('huge');
+            $('#back-overlay').css("background", "rgba(0,0,0,0.5)");
+            $('#container').css("background", "url('img/me.jpg')");
+            $('#container').css("background-position", "center");
+            $('#container').css("background-repeat", "none");
+            $('#container').css("background-size", "cover");
         }
         welcomeheight = ($(window).height() - $('.welcome').height())/2;
         $('.welcome').css('top', welcomeheight.toString()+"px");
