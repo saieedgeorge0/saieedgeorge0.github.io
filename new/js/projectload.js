9cba39654f485a7c4c1dd1efb098753830bb0937
George Saieed <saieed.george0@gmail.com>
2016-09-18 02:30:04 -0400

    ugh

--- new/js/projectload.js
+++ new/js/projectload.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
             $(window).on("load", function() {
-                $(".cssload-container").delay(2000).fadeOut(500);
+                $(".cssload-container").delay(1500).fadeOut(500);
             });
 });
