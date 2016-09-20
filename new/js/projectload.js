05ebd8a4cd7230c2574333210acfe5b0f4bd19b6
George Saieed <saieed.george0@gmail.com>
2016-09-19 21:08:47 -0400

    fix slow project loading

--- new/js/projectload.js
+++ new/js/projectload.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
             $(window).on("load", function() {
+                $("body").css("visibility", "visible");
                 $(".cssload-container").delay(1500).fadeOut(500);
             });
 });
