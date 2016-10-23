7f92844ff37e1bb209fbccf06fe086fccbe46720
George Saieed <saieed.george0@gmail.com>
2016-10-23 00:47:33 -0400

    blah

--- ER/js/script2.js
+++ ER/js/script2.js
@@ -18,4 +18,24 @@ $(document).ready(function(){
     $('#injury-report').click(function(event){
         window.location.href = "injury.html";
     });
+    
+    function getUrlVars() {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+    
+    $(window).on("load", function() {
+        if (getUrlVars()["submitted"]=="true") {
+            $('.ma-card-2').css("display", "block");
+            $('.ma-card-1').css("display", "none");
+            $('.confirm-card').css("height", "440px");
+            $('.confirm-card').css("margin-top", "-40px");
+        }
+    });
 });
