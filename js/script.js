cbe22fb246a7432bd12075a40d2d42168e311a6f
George Saieed <saieed.george0@gmail.com>
2016-11-10 18:23:42 -0600

    swiping works

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
     var blahblah = 0;
     var section = 1;
     
+    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)  { 
+        $('#blah').css("height", "-webkit-calc(100% - 75px)");
+    }
     
 
     if($(window).width() > 824) {
